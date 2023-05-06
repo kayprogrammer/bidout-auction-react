eb7ee80b1fbd7ba361f556d1691c1de5beaf2fcc
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-05-06 23:02:14 +0100

    added backdrop for category slide

--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -1,12 +1,11 @@
-import { Box, Button, Card, CloseButton, Heading, Slide } from '@chakra-ui/react'
+import { Box, Button, Card, CloseButton, Heading, Slide, ModalOverlay, Modal } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import '../App.css'
 
 const SubHeader = ({ name }) => {
   const current_path = window.location.pathname
-  const [isSideMenuOpen, setisSideMenuOpen] = useState(false);
-  
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+
   const categoryButtonStyles = {
     w: '100%',
     size: 'lg',
@@ -24,13 +23,18 @@ const SubHeader = ({ name }) => {
       <Box display={{ sm: 'block', md: 'flex' }} bgColor='rgb(220, 53, 69)' alignItems='center' p={{ base: '30px 30px 30px 30px', md: '58px 100px 50px 100px' }}>
         <Heading size='2xl' color='white'>{name}</Heading>
         {(current_path === '/listings' || current_path === '/listings/') && (
-          <Button mt={{ base: 5 }} colorScheme='blue' ml='auto' onClick={() => setisSideMenuOpen(!isSideMenuOpen)}>Categories</Button>
+          <Button mt={{ base: 5 }} colorScheme='blue' ml='auto' onClick={() => setIsSideMenuOpen(true)}>Categories</Button>
         )}
       </Box>
       {(current_path === '/listings' || current_path === '/listings/') && (
-        <Slide onClick={() => setisSideMenuOpen(!isSideMenuOpen)} direction='left' in={isSideMenuOpen} unmountOnExit={true} p='400px' style={{ "zIndex": '10' }} className={isSideMenuOpen ? 'dimmed-page' : ''}>
-          <Card width='25%' height='100%' p='1' onClick={(e) => e.stopPropagation()}>
-            <CloseButton size='lg' ml='auto' mt='2' onClick={() => setisSideMenuOpen(!isSideMenuOpen)} />
+        <Slide onClick={() => setIsSideMenuOpen(false)} direction='left' in={isSideMenuOpen} unmountOnExit={true} p='400px' style={{ "zIndex": 9999 }}>
+          {isSideMenuOpen && (
+            <Modal isOpen={true}>
+              <ModalOverlay />
+            </Modal>
+          )}
+          <Card width='400px' maxW='100%' height='100%' p='1' onClick={(e) => e.stopPropagation()}>
+            <CloseButton size='lg' ml='auto' mt='2' onClick={() => setIsSideMenuOpen(false)} />
             <Heading color='gray' mt={6} ml={3}>Categories</Heading>
             <Box p={7} pt={6}>
               <Button {...categoryButtonStyles}><Link>All</Link></Button>
