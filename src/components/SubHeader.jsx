0b9e8a63dcb27b6e2d628d5d5c26b72f4df074ca
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-05-06 01:12:12 +0100

    side category design

--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -1,10 +1,24 @@
-import { Box, Button, Card, Heading, Slide } from '@chakra-ui/react'
+import { Box, Button, Card, CloseButton, Heading, Slide } from '@chakra-ui/react'
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import '../App.css'
 
 const SubHeader = ({ name }) => {
   const current_path = window.location.pathname
   const [isSideMenuOpen, setisSideMenuOpen] = useState(false);
+  
+  const categoryButtonStyles = {
+    w: '100%',
+    size: 'lg',
+    h: '60px',
+    mb: 5,
+    bgColor: 'rgb(108, 117, 125)',
+    color: 'white',
+    borderRadius: 12,
+    _hover: { bg: 'rgb(108, 117, 125)' }
+  }
 
+  const categoryElements = [1, 2, 3, 4, 5]
   return (
     <>
       <Box display={{ sm: 'block', md: 'flex' }} bgColor='rgb(220, 53, 69)' alignItems='center' p={{ base: '30px 30px 30px 30px', md: '58px 100px 50px 100px' }}>
@@ -14,8 +28,17 @@ const SubHeader = ({ name }) => {
         )}
       </Box>
       {(current_path === '/listings' || current_path === '/listings/') && (
-        <Slide direction='left' in={isSideMenuOpen} unmountOnExit={true} p='400px' style={{ "zIndex": '10' }}>
-          <Card width='25%' height='100%'>
+        <Slide onClick={() => setisSideMenuOpen(!isSideMenuOpen)} direction='left' in={isSideMenuOpen} unmountOnExit={true} p='400px' style={{ "zIndex": '10' }} className={isSideMenuOpen ? 'dimmed-page' : ''}>
+          <Card width='25%' height='100%' p='1' onClick={(e) => e.stopPropagation()}>
+            <CloseButton size='lg' ml='auto' mt='2' onClick={() => setisSideMenuOpen(!isSideMenuOpen)} />
+            <Heading color='gray' mt={6} ml={3}>Categories</Heading>
+            <Box p={7} pt={6}>
+              <Button {...categoryButtonStyles}><Link>All</Link></Button>
+              {categoryElements.map((el) => (
+                <Button {...categoryButtonStyles}><Link>Category {el}</Link></Button>
+              ))}
+              <Button {...categoryButtonStyles}><Link>Other</Link></Button>
+            </Box>
 
           </Card>
         </Slide>
