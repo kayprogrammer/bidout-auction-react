fcff7655945f8ec55489e6c14e60d2db1003d5d9
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-05-10 14:54:15 +0100

    fixed header issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -63,14 +63,14 @@ const Header = () => {
       {!isLargeScreen && (
         <Slide backgroundColor="rgb(248, 249, 250)" direction="top" in={isMenuOpen} unmountOnExit={true} style={{ width: "100%", height: "100vh", zIndex: 1 }}>
           <Box pos="absolute" top="60px" left="0" right="0" backgroundColor="rgb(248, 249, 250)" p='1em 2.5em 2.5em 2.5em'>
-            <Link to='/' style={linkStyles2}>Home</Link>
-            <Link to='/listings' style={linkStyles2}>Active Listings</Link>
-            <Link to='/login' style={linkStyles2}>Login</Link>
-            <Link to='/signup' style={linkStyles2}>Sign Up</Link>
-            <Link to='/watchlist' style={linkStyles2}>Watch List</Link>
-            <Link to='/create-listing' style={linkStyles2}>Create Listing</Link>
+            <Link onClick={() => setIsMenuOpen(false)} to='/' style={linkStyles2}>Home</Link>
+            <Link onClick={() => setIsMenuOpen(false)} to='/listings' style={linkStyles2}>Active Listings</Link>
+            <Link onClick={() => setIsMenuOpen(false)} to='/login' style={linkStyles2}>Login</Link>
+            <Link onClick={() => setIsMenuOpen(false)} to='/signup' style={linkStyles2}>Sign Up</Link>
+            <Link onClick={() => setIsMenuOpen(false)} to='/watchlist' style={linkStyles2}>Watch List</Link>
+            <Link onClick={() => setIsMenuOpen(false)} to='/create-listing' style={linkStyles2}>Create Listing</Link>
 
-            <Button mt={2} backgroundColor="rgb(220, 53, 69)" _hover={{ bg: 'red.600' }} color='white' onClick={() => console.log("dashboard")}>My Dashboard</Button>
+            <Button mt={2} backgroundColor="rgb(220, 53, 69)" _hover={{ bg: 'red.600' }} color='white' onClick={() => { navigate('/dashboard'); setIsMenuOpen(false) }}>My Dashboard</Button>
           </Box>
         </Slide>
       )}
