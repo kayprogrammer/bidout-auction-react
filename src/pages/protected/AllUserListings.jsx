d86903065204ec5582881ce4b962048720b0a909
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-05-28 23:03:51 +0100

    all auctioneer listings data fetching

--- src/pages/protected/AllUserListings.jsx
+++ src/pages/protected/AllUserListings.jsx
@@ -1,13 +1,26 @@
 import { Box, Image, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
-import React from 'react'
-import { SubHeader } from '../../components'
+import React, { useEffect } from 'react'
+import { Spinner, SubHeader } from '../../components'
 import { Link, useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit } from '@fortawesome/free-solid-svg-icons'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAuctioneerListings } from '../../features/listings/listingsSlice'
 
 const AllUserListings = () => {
-    const tableElements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     const navigate = useNavigate();
+    const { listings, isLoading } = useSelector((state) => state.listings)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getAuctioneerListings());
+    }, [dispatch])
+
+    const handleListingImageError = (event) => {
+        event.target.src = 'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'; // Replace with your fallback image link
+    };
+
+    if (isLoading) return <Spinner />;
 
     return (
         <>
@@ -27,15 +40,15 @@ const AllUserListings = () => {
                             </Tr>
                         </Thead>
                         <Tbody>
-                            {tableElements.map((el) => (
-                                <Tr>
-                                    <Td>1</Td>
-                                    <Td>Brand New royal Enfield 250 CC For special Sale</Td>
-                                    <Td maxW='100%' w='15%'><Image src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80' /></Td>
-                                    <Td>$1000</Td>
+                            {listings.map((listing, i) => (
+                                <Tr key={i}>
+                                    <Td>{i + 1}</Td>
+                                    <Td>{listing.name}</Td>
+                                    <Td maxW='100%' w='15%'><Image src={listing.image} onError={handleListingImageError} /></Td>
+                                    <Td>${listing.price}</Td>
                                     <Td>Active</Td>
-                                    <Td><Link to='/dashboard/listings/1/bids'>1</Link></Td>
-                                    <Td role='button' onClick={() => navigate(`/dashboard/listings/${el}/update`)}><FontAwesomeIcon icon={faEdit} /></Td>
+                                    <Td><Link to='/dashboard/listings/1/bids'>{listing.bids_count}</Link></Td>
+                                    <Td role='button' onClick={() => navigate(`/dashboard/listings/${listing.slug}/update`)}><FontAwesomeIcon icon={faEdit} /></Td>
                                 </Tr>
                             ))}
                         </Tbody>
